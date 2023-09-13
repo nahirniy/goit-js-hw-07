b749ad8049954def3b1245fd15c463dbd4921c76
nahirniy <nahirniyy@gmail.com>
2023-09-13 23:11:12 +0300

    hw 7 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,22 +2,49 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const imageContainer = document.querySelector(".gallery");
+let imageModule;
 
-let idCounter = 0;
-const markup = galleryItems.map(
-    ({ preview: smallImage, original: bigImage, description }) =>
-        `<li data-id="${(idCounter += 1)}" class="gallery__item js-item"><img class="gallery__image js-image" src="${smallImage}" alt="${description}" /></li>`
-);
-imageContainer.insertAdjacentHTML("beforeend", markup.join(""));
+// 1) create markup for HTML
+const markup = galleryItems
+    .map(
+        ({ preview, original, description }) =>
+            `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>`
+    )
+    .join("");
+imageContainer.insertAdjacentHTML("beforeend", markup);
 
+// 2) create modal images with basicLightbox library
 function onClick(e) {
+    e.preventDefault();
     const targetImage = e.target;
-    if (!targetImage.classList.contains("js-image")) {
+    if (!targetImage.classList.contains("gallery__image")) {
         return;
     }
+    const currentImage = targetImage.dataset.source;
+    const currentImageDescription = targetImage.getAttribute("alt");
 
-    const imageId = targetImage.closest(".js-item").dataset.id;
-    console.log(imageId);
+    imageModule = basicLightbox.create(
+        `<img src="${currentImage}" alt="${currentImageDescription}">`
+    );
+
+    imageModule.show();
+}
+
+// 3) close modal images with escape
+function onEscape(e) {
+    if (e.code === "Escape") {
+        imageModule.close();
+    }
 }
 
 imageContainer.addEventListener("click", onClick);
+imageContainer.addEventListener("keydown", onEscape);
